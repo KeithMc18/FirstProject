b67e419bb1bd9f7e2c54be828955407532e2b04e
Keith McGloin <keithmc1810@gmail.com>
2019-02-05 11:32:30 +0000

    Classes divided into modules

--- classes.py
+++ classes.py
@@ -1,15 +1,2 @@
-students = []
 
-
-class Student:
-    def __init__(self, name, student_id=1232):
-        student = {"name": name, "student_id": student_id}
-        students.append(student)
-
-    def __str__(self):
-        return "Student"
-
-
-mark = Student("Mark")
-print(mark)
 
