31e83b95b36d44cb234f52444e47235eaf6d3a52
Keith McGloin <keithmc1810@gmail.com>
2019-02-05 07:21:52 +0000

    Save to file and read from file

--- function.py
+++ function.py
@@ -1,20 +1,53 @@
 students = []
 
+# enter_student = input("Do you want to enter a student name? Y/N")
+# yes = "Y"
 
 def get_student_titlecase():
     student_titlecase = []
     for student in students:
-        student_titlecase = student.title()
+        student_titlecase.append(student["name"].title()) #, student["student_id"].title()
     return student_titlecase
 
+
 def print_student_titlecase():
-    student_titlecase = []
-    for student in students:
-        student_titlecase = student.title()
+    student_titlecase = get_student_titlecase()
     print(student_titlecase)
 
 
-def add_student(name):
-    students.append(name)
+def add_student(name, student_id=1232):
+    student = {"name": name, "student_id": student_id}
+    students.append(student)
 
-student_list = get_student_titlecase()
+
+def save_file(student):
+    try:
+        f = open("student.txt", "a")
+        f.write(student + "\n")
+        f.close()
+    except Exception:
+        print("Could not save file")
+
+
+def read_file():
+    try:
+        f = open("student.txt", "r")
+        for student in f.readline():
+            add_student(student)
+        f.close()
+    except Exception:
+        print("Could not read file")
+
+
+read_file()
+print_student_titlecase()
+
+# if enter_student == "y" or enter_student == "Y":
+student_name = input("Enter a students name: ")
+student_id = input("Enter the student id: ")
+
+add_student(student_name, student_id)
+save_file(student_name)
+
+# else:
+#     print("You have exited the programme. Thank You")
